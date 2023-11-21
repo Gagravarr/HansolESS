15e98f951518e1a1d10427dcd3644108d40ee20c
Nick Burch <code@gagravarr.org>
2023-11-21 12:36:35 +0000

    Extract voltage/current/power table

--- extract.py
+++ extract.py
@@ -32,7 +32,27 @@ def extract_4col(table):
             data[d.text] = v.text
    return data
 
+def extract_vip(table):
+   "Extract the Voltage/Current/Power data"
+   data = {}
+   for row in table.find_all("tr"):
+      label = row.find("td").text
+      vip = {}
+
+      cells = iter( row.find_all("td")[1:] )
+      for d, v in zip(cells,cells):
+         if d.text == "V[V]:":
+            vip["voltage"] = float(v.text.strip())
+         if d.text == "I[A]:":
+            vip["current"] = float(v.text.strip())
+         if d.text == "P[W]:":
+            vip["power"] = float(v.text.strip())
+      if len(vip) == 3:
+         data[label] = vip
+   return data
+
 o = soup.find("td", string=heading_overall)
 p = soup.find("td", string=heading_pvbat)
 
 print(extract_4col(o.parent.parent))
+print(extract_vip(p.parent.parent))
