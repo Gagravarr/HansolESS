4603a33281de43fe079f695d7a9ae3ffdb871065
Nick Burch <code@gagravarr.org>
2023-11-21 12:26:12 +0000

    Fix BS parser warning, extract 4-col table

--- extract.py
+++ extract.py
@@ -8,13 +8,31 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 url = "http://10.5.2.80:21710/F0"
+
+try:
+   import lxml
+   parser = "lxml"
+except ModuleNotFoundError:
+   parser = "html.parser"
+
 with urlopen(url) as page:
-   soup = BeautifulSoup(page)
-# TODO see if we can do lxml, otherwise html.parser backup
+   soup = BeautifulSoup(page, parser)
 
 heading_overall = "EMS Control MODE"
 heading_pvbat   = "PCS Sensing Data"
 
-print(soup)
-t = soup.find("td", string=heading_overall)
-print(t)
+def extract_4col(table):
+   "Get the labels and values for a 4-column table, excluding the header"
+   data = {}
+   for row in table.find_all("tr")[1:]:
+      cells = iter( row.find_all("td") )
+      for d, v in zip(cells,cells):
+         if d and v and d.text and v.text:
+            # TODO ints and floats
+            data[d.text] = v.text
+   return data
+
+o = soup.find("td", string=heading_overall)
+p = soup.find("td", string=heading_pvbat)
+
+print(extract_4col(o.parent.parent))
