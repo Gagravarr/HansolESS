6e6ed10aab192da3118f4831abdc40a96fc92c99
Nick Burch <code@gagravarr.org>
2023-11-22 14:40:26 +0000

    repr method for VIP

--- models.py
+++ models.py
@@ -2,15 +2,21 @@
 
 class VIP:
    "Voltage (V), Current (I) and Power (P), for the different components"
+   attrs = ("voltage","current","power")
+
    def __init__(self, component, voltage=None, current=None, power=None):
       self.component = component
       self.voltage = voltage
       self.current = current
       self.power = power
-   def items(self):
-      return [(k,getattr(self,k)) for k in ("voltage","current","power")]
+
    def is_complete(self):
       return self.voltage != None and self.current != None and self.power != None
+   def items(self):
+      return [(k,getattr(self,k)) for k in VIP.attrs]
+   def __repr__(self):
+      return "%s " % (self.component) + \
+             ",".join(["%s=%0.1f" % (k,v) for k,v in self.items()])
 
 class Battery:
    "Battery state"
